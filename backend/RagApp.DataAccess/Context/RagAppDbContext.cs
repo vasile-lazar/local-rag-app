f65f035ff506b83067cd53eb5597388d4a443396
Vasile Lazar <vlazar184@gmail.com>
2026-09-04 11:21:46 +0300

    Add DbContext and Migrations
    Add RagAppDbContext that holds all DbSets and owns the ModelBuilder
    Add Migrations for the db tables

---
+++ backend/RagApp.DataAccess/Context/RagAppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RagApp.Domain.Entities.Conversation;
+using RagApp.Domain.Entities.Message;
+using RagApp.Domain.Entities.User;
+
+namespace RagApp.DataAccess.Context;
+
+public class RagAppDbContext : DbContext
+{
+    public RagAppDbContext(DbContextOptions<RagAppDbContext> options) : base(options) { }
+
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<ConversationEntity> Conversations { get; set; }
+    public DbSet<MessageEntity> Messages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserEntity>()
+            .Property(u => u.Role)
+            .HasConversion<string>();
+
+        modelBuilder.Entity<UserEntity>()
+            .Property(u => u.Status)
+            .HasConversion<string>();
+
+        modelBuilder.Entity<MessageEntity>()
+            .Property(m => m.Role)
+            .HasConversion<string>();
+
+        modelBuilder.Entity<UserEntity>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+    }
+}
---
+++ backend/RagApp.DataAccess/Migrations/20260902124223_UpdateMessage.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RagApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ErrorSeverity",
+                table: "Messages",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsError",
+                table: "Messages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ErrorSeverity",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "IsError",
+                table: "Messages");
+        }
+    }
+}
